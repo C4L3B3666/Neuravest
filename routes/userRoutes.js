cfa5c66220f7d9d8c12cc2091215f393ee7c9ab6
C4L3B3666 <calebec055@gmail.com>
2025-06-25 23:41:25 +0100

    Correção nos erros na rotas

---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('./models/User');
+
+// Rota de cadastro
+router.post('/registrar', async (req, res) => {
+  const { nomeCompleto, nomeFicticio, telefone, senha } = req.body;
+
+  try {
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    const novoUsuario = new User({
+      nomeCompleto,
+      nomeFicticio,
+      telefone,
+      senha: senhaHash
+    });
+
+    await novoUsuario.save();
+    res.status(201).send('Cadastrado com Sucesso!');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao criar conta.');
+  }
+});
+
+module.exports = router;
